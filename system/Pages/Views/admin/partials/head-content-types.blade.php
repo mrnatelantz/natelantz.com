5b67548dc0b3c9b765da14c6d74478cc0a9fa4b7
Nate Lantz <mrnatelantz@gmail.com>
2016-03-04 09:11:51 -0800

    remove un-needed code

--- system/Pages/Views/admin/partials/head-content-types.blade.php
+++ system/Pages/Views/admin/partials/head-content-types.blade.php
@@ -8,18 +8,15 @@
         @if(isset($page) && isset($page->head) && !is_null($page->head))
             @foreach($page->head as $contentTypes)
                 @foreach($contentTypes as $type => $content)
-                    <?php $orderCount = 0; ?>
                     @if(View::exists('admin.content-types.head.'.$type))
                         <?php $contentType = [
                                 'content' => $content,
-                                'orderByCount' => $orderCount++,
                                 'ajax' => false
                         ]; ?>
                         @include('admin.content-types.head.'.$type, ['contentType' => $contentType])
                     @elseif(View::exists('pages::admin.content-types.head.'.$type))
                         <?php $contentType = [
                                 'content' => $content,
-                                'orderByCount' => $orderCount++,
                                 'ajax' => false
                         ]; ?>
                         @include('pages::admin.content-types.head.'.$type, ['contentType' => $contentType])
@@ -30,14 +27,12 @@
             @if(View::exists('admin.content-types.head.meta-description'))
                 <?php $contentType = [
                         'content' => '',
-                        'orderByCount' => 0,
                         'ajax' => false
                 ]; ?>
                 @include('admin.content-types.head.meta-description', ['contentType' => $contentType])
             @elseif(View::exists('pages::admin.content-types.head.meta-description'))
                 <?php $contentType = [
                         'content' => '',
-                        'orderByCount' => 0,
                         'ajax' => false
                 ]; ?>
                 @include('pages::admin.content-types.head.meta-description', ['contentType' => $contentType])
@@ -46,14 +41,12 @@
             @if(View::exists('admin.content-types.head.meta-keywords'))
                 <?php $contentType = [
                         'content' => '',
-                        'orderByCount' => 0,
                         'ajax' => false
                 ]; ?>
                 @include('admin.content-types.head.meta-keywords', ['contentType' => $contentType])
             @elseif(View::exists('pages::admin.content-types.head.meta-keywords'))
                 <?php $contentType = [
                         'content' => '',
-                        'orderByCount' => 0,
                         'ajax' => false
                 ]; ?>
                 @include('pages::admin.content-types.head.meta-keywords', ['contentType' => $contentType])
