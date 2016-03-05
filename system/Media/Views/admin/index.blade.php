b3e69502bcf4b6e22d99afc59a1cab019ad96220
Nate Lantz <mrnatelantz@gmail.com>
2016-03-04 16:05:33 -0800

    add placeholder vuejs data

--- system/Media/Views/admin/index.blade.php
+++ system/Media/Views/admin/index.blade.php
@@ -1,5 +1,54 @@
 @extends('media::admin.layouts.app')
 
 @section('body')
-    <h3>Media</h3>
+    <div id="media">
+        <template v-for="item in media">
+            <div class="col-md-3">
+                <img src="@{{ item.src }}" class="img-circle img-responsive" style="padding-bottom: 10px;"/>
+            </div>
+        </template>
+    </div>
 @endsection
+
+
+@push('scripts')
+    <script src="//cdn.jsdelivr.net/vue/1.0.17/vue.min.js"></script>
+
+    <script>
+        new Vue({
+            el: '#media',
+            data: {
+                media: [
+                    {src: "http://placehold.it/300x300"},
+                    {src: "http://placehold.it/300x300"},
+                    {src: "http://placehold.it/300x300"},
+                    {src: "http://placehold.it/300x300"},
+                    {src: "http://placehold.it/300x300"},
+                    {src: "http://placehold.it/300x300"},
+                    {src: "http://placehold.it/300x300"},
+                    {src: "http://placehold.it/300x300"},
+                    {src: "http://placehold.it/300x300"},
+                    {src: "http://placehold.it/300x300"},
+                    {src: "http://placehold.it/300x300"},
+                    {src: "http://placehold.it/300x300"}
+                ]
+                /*
+                media: function(){
+                    var data;
+                    for(var i = 0; i < 10; i++) {
+                        data[i] = {
+                            name: i,
+                            src: "http://placehold.it/300x300"
+                        }
+                    }
+                    console.log(data);
+                    return data;
+                }
+                */
+            },
+            methods: {
+
+            }
+        })
+    </script>
+@endpush
---
+++ tests/RadCms/Media/MediaTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class MediaTest extends TestCase
+{
+
+    /** @test */
+    function it_finds_a_media_item()
+    {
+
+    }
+}
